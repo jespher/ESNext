d57484083be8ebf2714f7408266c9370f2ce03ac
Jefferson Luis <luis.88.rasini@gmail.com>
2022-03-08 12:29:37 -0300

    Ajuste no código

--- 01 - revisao1.js
+++ 01 - revisao1.js
@@ -1,22 +1,22 @@
 // let e const
-// {
-//     var a = 2
-//     let b = 3
-//     console.log(b)
-// }
-//
-// console.log(a)
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+
+console.log(a)
 
 // Template String
-// const produto = 'iPad'
-// console.log(`${produto} é caro!`)
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
 
 // Destructuring
-// const [l, e, ...tras] = 'Cod3r'
-// console.log(l, e, tras)
+const [l, e, ...tras] = 'Cod3r'
+console.log(l, e, tras)
 
-// const [x, , y] = [1, 2, 3]
-// console.log(x, y)
+const [x, , y] = [1, 2, 3]
+console.log(x, y)
 
 const { idade, nome: n } = { nome: 'Diebersan,', idade: 36 }
 console.log(n, idade + ' anos de idade.')
